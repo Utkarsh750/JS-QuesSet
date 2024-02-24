fe8e22d5b994491ec20676048acff2746b5231d2
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-02-24 23:03:40 +0530

    solved filter method questions

--- functionalProgSet1.js
+++ functionalProgSet1.js
@@ -2,7 +2,7 @@
 const incrementNumbers = (numbers) => {
   return numbers.map((num) => num + 3);
 };
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
 // console.log(incrementNumbers(numbers));
 
@@ -13,17 +13,17 @@ const capitalizeArray = (myArray) => {
 const myArray = ["apple", "banana", "cherry"];
 // console.log(capitalizeArray(myArray));
 
-// ques - 3
-const getAges = (people) => {
-  return people.map((data) => data.age);
-};
-const people = [
-  { name: "Ankit", age: 25 },
-  { name: "Vinit", age: 24 },
-  { name: "Shashi", age: 29 },
-];
-const ages = getAges(people);
-// console.log(ages);
+// // ques - 3
+// const getAges = (people) => {
+//   return people.map((data) => data.age);
+// };
+// const peoples = [
+//   { name: "Ankit", age: 25 },
+//   { name: "Vinit", age: 24 },
+//   { name: "Shashi", age: 29 },
+// ];
+// const ages = getAges(people);
+// // console.log(ages);
 
 // ques - 4
 const getProductNamesInLowerCase = (products) => {
@@ -46,4 +46,33 @@ const cities = [
   { name: "Bangalore", population: 13608000 },
 ];
 
-console.log(getCityNames(cities));
+// console.log(getCityNames(cities));
+
+// ques - 6
+const filterLongStrings = (fruits) => {
+  return fruits.filter((fruit) => fruit.length > 5);
+};
+const fruits = ["apple", "banana", "cherry", "watermelon", "pineapple"];
+// console.log(filterLongStrings(fruits));
+
+// ques 7
+const isDivisibleBy3and5 = (numbers) => {
+  return numbers.filter((num) => num % 3 == 0 && num % 5 == 0);
+};
+const numbers = [1, 2, 30, 4, 15];
+// console.log(isDivisibleBy3and5(numbers));
+
+// ques - 8
+const filterByAge = (people) => {
+  return people.filter((peoples) => peoples.age > 30);
+};
+const people = [
+  { name: "Rahul", age: 25 },
+  { name: "Raj", age: 35 },
+  { name: "Vijay", age: 45 },
+];
+
+// console.log(filterByAge(people));
+
+// ques - 9
+
