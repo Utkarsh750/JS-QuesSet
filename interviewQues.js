719e36d54e6fc260ee49cef4b16ebf22d2c6a1bc
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-03-21 03:45:41 +0530

    added new questin

--- interviewQues.js
+++ interviewQues.js
@@ -14,3 +14,17 @@ var b = 2;
 
 var y = 1000;
 console.log(y);
+
+function sumA(a) {
+  return function sumB(b) {
+    return function sumC(c) {
+      console.log(a, b, c);
+
+      return a + b + c;
+    };
+  };
+}
+
+sumA(2)(3)(4);
+
+console.log("noth");
