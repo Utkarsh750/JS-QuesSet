299ba97e4fade6dcce8f3e69f80f23a27fbda4e9
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-04-14 22:37:15 +0530

    added new pollyfil for map

--- interviewQues.js
+++ interviewQues.js
@@ -1,30 +1,53 @@
-const userData = {
-  firstName: "Utkarsh",
-  lastName: "Sharma",
-  age: 25,
-  printData: function () {
-    console.log(this.firstName + " " + this.lastName);
-  },
+// const userData = {
+//   firstName: "Utkarsh",
+//   lastName: "Sharma",
+//   age: 25,
+//   printData: function () {
+//     console.log(this.firstName + " " + this.lastName);
+//   },
+// };
+
+// userData.printData();
+
+// var a = 1;
+// var b = 2;
+
+// var y = 1000;
+// console.log(y);
+
+// function sumA(a) {
+//   return function sumB(b) {
+//     return function sumC(c) {
+//       console.log(a, b, c);
+
+//       return a + b + c;
+//     };
+//   };
+// }
+
+// sumA(2)(3)(4);
+
+// Polyfill for map()
+Array.prototype.myMapArray = function (cb) {
+  let temp = []; // to create new array we require a empty array so we took this
+
+  for (let i = 0; i < this.length; i++) {
+    temp.push(cb(this[i], i, this));
+  }
+
+  return temp;
 };
 
-userData.printData();
+const nums = [2, 4, 6, 8];
+const multiThree = nums.myMapArray((num, i, arr) => {
+  return num * 3;
+});
 
-var a = 1;
-var b = 2;
+console.log(multiThree);
 
-var y = 1000;
-console.log(y);
 
-function sumA(a) {
-  return function sumB(b) {
-    return function sumC(c) {
-      console.log(a, b, c);
+// Pollyfill for filter
 
-      return a + b + c;
-    };
-  };
+Array.prototype.myFilter = function(cb) {
+  
 }
-
-sumA(2)(3)(4);
-
-console.log("noth");
