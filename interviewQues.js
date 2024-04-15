c1beafcfffba3e5371d28a7a4c1250c56c1735c8
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-04-15 13:22:41 +0530

    added pollyfill for filter

--- interviewQues.js
+++ interviewQues.js
@@ -38,16 +38,29 @@ Array.prototype.myMapArray = function (cb) {
   return temp;
 };
 
-const nums = [2, 4, 6, 8];
+// const nums = [2, 4, 6, 8];
 const multiThree = nums.myMapArray((num, i, arr) => {
   return num * 3;
 });
 
 console.log(multiThree);
 
-
 // Pollyfill for filter
 
-Array.prototype.myFilter = function(cb) {
-  
-}
+Array.prototype.myFilter = function (cb) {
+  let temp = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      temp.push(this[i]);
+    }
+  }
+  return temp;
+};
+
+const numsFilter = [1, 2, 3, 4];
+const moreThanTwo = numsFilter.myFilter((num) => num > 2);
+
+console.log(moreThanTwo);
+
+// Pollyfill for reduce
