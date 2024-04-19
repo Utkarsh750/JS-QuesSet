a322be7bf50008db1322f91e47fa86045d52c824
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-04-19 13:33:12 +0530

    added fibonacci

--- DSAQuestion.js
+++ DSAQuestion.js
@@ -1,8 +1,21 @@
 // Palindrom Number
 
 const palindrom = (x) => {
-  return x === +x.toString().split("").reverse().join("");
+  return x > 0 ? false : x === +x.toString().split("").reverse().join("");
 };
 
 const result = palindrom(98);
-console.log(result);
+// console.log(result);
+
+// Finbonacci Series
+
+const fib = (n) => {
+  const arr = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  console.log(arr[n]);
+};
+
+fib(7);
