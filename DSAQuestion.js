65e9477b177f8971f6d1b0cfab21f2f77e293ad2
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-06-25 02:06:09 +0530

    added new DSA question

--- DSAQuestion.js
+++ DSAQuestion.js
@@ -19,3 +19,15 @@ const fib = (n) => {
 };
 
 // fib(7);
+
+// SUM OF DIGITS OF NUMBER
+function sumOfDigits(num) {
+  let sum = 0;
+  while (num > 0) {
+    sum = sum + (num % 10);
+    num = Math.floor(num / 10);
+  }
+  return sum;
+}
+
+console.log(sumOfDigits(1234));
