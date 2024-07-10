6cdba2b50c7467bbd4d7963a4bf82b2bc239af96
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-07-10 20:02:04 +0530

    added new question of async-await

--- asynSet1.js
+++ asynSet1.js
@@ -8,19 +8,30 @@ const delayedGreeting = () => {
 // delayedGreeting();
 
 // Ques 2
-const delayedAddition = (a,b) => {
-    setTimeout(() => {
-        console.log("sum is" , a + b)
-    },4000)
-}
+const delayedAddition = (a, b) => {
+  setTimeout(() => {
+    console.log("sum is", a + b);
+  }, 4000);
+};
 
 // delayedAddition(2,3);
 
 // Ques 3
 const delayAlert = (msg, time) => {
-    setTimeout(() => {
-       alert(msg);
-    }, time);
-}
+  setTimeout(() => {
+    alert(msg);
+  }, time);
+};
 
-delayAlert("Hello World", 2000)
+// delayAlert("Hello World", 4000)
+
+// Ques 4
+const delayedLoop = (num) => {
+  for (let i = 0; i < num; i++) {
+    setTimeout(() => {
+      console.log("Hello");
+    }, (i + 1) * 1000);
+  }
+};
+
+delayedLoop(3);
